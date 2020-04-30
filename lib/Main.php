019271712bdce3951e075ff1542b48dab42daffc
vagrant <vagrant@localhost.localdomain>
2020-04-30 14:54:18 +0000

    Library Files

--- lib/Main.php
+++ lib/Main.php
@@ -6,8 +6,14 @@ class Main {
   protected $db;
 
   public function __construct() {
-    $this->db = new \PDO(DSN);
-    $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    try {
+      $this->db = new \PDO(DSN);
+      $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+      $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    } catch (\PDOException $e) {
+      $e->getMessage();
+      exit;
+    }
+
   }
 }
